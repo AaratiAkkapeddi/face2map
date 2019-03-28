73f1967f1e9056623be07b66c8a0465f4a1b5172
Aarati <aarati@Aaratis-MBP-2.home>
2019-03-28 13:54:45 -0400

    first commit

---
+++ face2map.py
@@ -0,0 +1,56 @@
+'''
+========================
+Face Vector to Topographic Map
+========================
+
+'''
+from mpl_toolkits.mplot3d import axes3d
+import numpy as np
+import sys
+import matplotlib
+from matplotlib import cm
+import face_recognition
+from natsort import natsorted, ns
+import matplotlib.pyplot as plt
+import matplotlib.tri as mtri
+from scipy.interpolate import griddata
+import scipy
+import math
+import os
+
+def group(lst, n):
+  for i in range(0, len(lst), n):
+    val = lst[i:i+n]
+    if len(val) == n:
+      yield tuple(val)
+
+def write_face(filename):
+    face = face_recognition.load_image_file(filename)
+    try:
+        gotdata = face_recognition.face_encodings(face)[0]
+        face_encoding = face_recognition.face_encodings(face)[0]
+        encoding = list(group(face_encoding, 4))
+        x = []
+        y = []
+        z = []
+        for i in encoding:
+            x.append(i[0])
+            y.append(i[1])
+            z.append(i[2] * i[3] * 100)
+        fig = plt.figure()
+        ax = fig.add_subplot(1,1,1, projection='3d')
+        ax._axis3don = False
+        ax.tricontour(x, y, z,15, linewidths=[0.5], colors='k')
+        ax.view_init(azim=0, elev=90)
+        plt.axis([-0.08,0.08,-0.08,0.08])
+        plt.show()
+     
+
+    except IndexError:
+        print('no-face')
+
+
+print(sys.argv[1])
+write_face(sys.argv[1])
+
+
---
+++ readme.md
@@ -0,0 +1,40 @@
+////////////////////////////////////////////////
+Dependencies: //////////////////////////////
+Python ////////////////////////////////////////////////
+Opencv ////////////////////////////////////////////////
+face recognition library by ageitgey //////////////////
+https://github.com/ageitgey/face_recognition //////////
+
+FACE -> Map <3
+
+Install Homebrew:
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+$ brew update
+
+Install Cmake
+brew install cmake
+brew install boost
+
+Install Face Recognition
+pip install face_recognition
+pip install natsort
+
+
+
+-----
+
+Machine Learning Resources:
+http://ml4a.github.io/
+
+
+
+Some projects for inspo:
+http://www.sarahmeyohas.com/rosesatbell
+https://artsexperiments.withgoogle.com/xdegrees/8gHu5Z5RF4BsNg/BgHD_Fxb-V_K3A/
+https://www.youtube.com/watch?v=yzJk6ww3LD0
+https://3milychu.github.io/pages/kmeansfashion.html
+https://glitch.com/@blprnt
+http://www.adrianaramic.com/
+http://dinakelberman.tumblr.com/
+
+
